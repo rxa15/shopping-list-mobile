d4c5cc2c914f6682869e3f35268bd2de2fd1a5e2
rxa15 <114054405+rxa15@users.noreply.github.com>
2023-11-15 10:00:42 +0700

    Revert "Done Tugas 8 PBP (without BONUS)"
    This reverts commit b2ecf5c2c9c1df0ef6b19ed41f1827869fb71460.

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:shopping_list/screens/menu.dart';
 import 'package:shopping_list/widgets/left_drawer.dart';
 import 'package:shopping_list/widgets/shop_card.dart';
 
@@ -46,59 +48,6 @@ class MyHomePage extends StatelessWidget {
                 children: items.map((ShopItem item) => ShopCard(item)).toList(),
               )
             ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ShopCard extends StatelessWidget {
-  final ShopItem item;
-
-  const ShopCard(this.item, {super.key}); // Constructor
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Colors.indigo,
-      child: InkWell(
-        // Area responsive terhadap sentuhan
-        onTap: () {
-          // Memunculkan SnackBar ketika diklik
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-                content: Text("Kamu telah menekan tombol ${item.name}!")));
-
-          if (item.name == "Tambah Produk") {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const ShopFormPage(),
-                ));
-          }
-        },
-        child: Container(
-          // Container untuk menyimpan Icon dan Text
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
           ),
         ),
       ),
